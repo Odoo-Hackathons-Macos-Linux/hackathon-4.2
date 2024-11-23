97f58ae9341670f2ad8fa2d3bfbd9c1048e19be3
Spat <Nicolas.Lasch@gmail.com>
2024-11-23 16:50:08 +0100

    saved UUID instead of sockets

--- src/game.js
+++ src/game.js
@@ -1,7 +1,4 @@
-import { Server } from 'socket.io';
-
 class GameServer {
-
   /**
    * @private
    */
@@ -13,53 +10,72 @@ class GameServer {
   turnNumber = 0;
 
   /**
-   * @type {number}
+   * @type {Object} - Tracks each player's choices and stats
    */
-  playersAlive;
+  playersStats = {};
 
   /**
-   * @type {Array<number>}
+   * @constructor
+   * @param {Server} server
    */
-  events;
-
-  /**
-   * @type {number}
-   */
-  currentEvent;
-
-  /**
-  * @constructor
-  * @param {Server} server
-  */
   constructor(server) {
     this.server = server;
   }
 
+  /**
+   * Reset all player stats when starting a new game or round
+   */
+  resetStats() {
+    this.playersStats = {};  // Clear all player data
+    console.log("Player stats have been reset");
+  }
+
+  /**
+   * Send the new turn to all clients
+   */
   sendNewTurn() {
     this.server.emit("newTurn", this.turnNumber);
     this.turnNumber += 1;
   }
 
   /**
-   * @param {string} id
+   * Handle when a player plays their turn
+   * @param {string} userId - The unique user identifier
+   * @param {number} turn - The current turn number
+   * @param {string} choice - The player's choice
    */
-  sendDead(id) {
-    const socket = this.server.sockets.sockets.get(id);
-    if (socket) {
-      socket.emit("dead");
+  onPlayerPlayed(userId, turn, choice) {
+    console.log(`Player ${userId} has played ${choice} at turn ${turn}`);
+
+    // If the player doesn't exist in the stats object, initialize them
+    if (!this.playersStats[userId]) {
+      this.playersStats[userId] = {
+        choices: [],
+        dead: false
+      };
     }
+
+    this.playersStats[userId].choices.push({ turn, choice });
+  }
+
+  /**
+   * Get all players and their choices
+   * @returns {Object} - The players' choices and stats
+   */
+  getAllPlayerStats() {
+    return this.playersStats;
+  }
+
+  /**
+   * Start the game (round 0)
+   * Reset stats at the start of the game or round
+   */
+  startGame() {
+    this.resetStats();  // Reset stats when starting a new game
+    console.log("Game has started! All player stats are reset.");
   }
 
   sendEndGame() { }
-
-  /**
-   * @param {string} id
-   * @param {number} turn
-   * @param {number} choice
-  */
-  onPlayerPlayed(id, turn, choice) {
-    console.log(`Client ${id} has played ${choice} at turn ${turn}`);
-  }
 }
 
 export default GameServer;
