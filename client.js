051442180db7e61c4f8a3a29069be6a7e58f769b
Jocke. <jockerider199@gmail.com>
2024-11-23 10:35:03 +0100

    poire (#1)

--- client.js
+++ client.js
@@ -4,4 +4,6 @@ const socket = io("http://localhost:3000");
 
 socket.on('connect', () => {
   console.log('Connected to server with ID:', socket.id);
+
+  socket.emit("turnChoice", 1, Math.floor(Math.random() * 4));
 });
